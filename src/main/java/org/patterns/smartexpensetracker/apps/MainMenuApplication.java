9155c2844d56144d5412eb12729c45fe300a593a
Daiyan Abrar Samin <daiyan03samin@gmail.com>
2025-11-28 19:08:27 -0500

    Minor change
    Altering minor change in MainMenuApplication temporarily before merging into main branch

--- src/main/java/org/patterns/smartexpensetracker/apps/MainMenuApplication.java
+++ src/main/java/org/patterns/smartexpensetracker/apps/MainMenuApplication.java
@@ -3,7 +3,6 @@ package org.patterns.smartexpensetracker.apps;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.patterns.smartexpensetracker.controllers.MainMenuController;
 import org.patterns.smartexpensetracker.views.MainMenuView;
 
 import java.io.IOException;
@@ -13,7 +12,6 @@ public class MainMenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         MainMenuView menuView = new MainMenuView();
-        new MainMenuController(menuView, stage);
 
         Scene scene = new Scene(menuView, 400, 300);
         stage.setTitle("Smart Expense Tracker");
