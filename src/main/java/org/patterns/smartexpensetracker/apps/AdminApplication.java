cfd640c54043c666977bff786ab4a99424696fa2
Mohamed Amine <m.a.hamidouch1@gmail.com>
2025-12-06 09:36:47 -0500

    Update AdminApplication.java

--- src/main/java/org/patterns/smartexpensetracker/apps/AdminApplication.java
+++ src/main/java/org/patterns/smartexpensetracker/apps/AdminApplication.java
@@ -11,7 +11,7 @@ public class AdminApplication extends Application {
 
     @Override
     public void start(Stage stage) {
-        AdminView view = new AdminView(); // NO ARGUMENTS
+        AdminView view = new AdminView();
         Scene scene = new Scene(view, 1000, 650);
         stage.setScene(scene);
         stage.setTitle("Admin Panel");
@@ -22,34 +22,3 @@ public class AdminApplication extends Application {
         launch();
     }
 }
-
-
-
-
-//package org.patterns.smartexpensetracker.apps;
-//
-//import javafx.application.Application;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//import org.patterns.smartexpensetracker.controllers.AdminController;
-//import org.patterns.smartexpensetracker.controllers.UserController;
-//import org.patterns.smartexpensetracker.views.AdminView;
-//
-//public class AdminApplication extends Application {
-//
-//    @Override
-//    public void start(Stage stage) {
-//        UserController userController = new UserController();
-//        AdminView view = new AdminView();
-//        new AdminController(view, userController);
-//
-//        Scene scene = new Scene(view, 1000, 650);
-//        stage.setTitle("Admin Panel");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public static void main(String[] args) {
-//        launch();
-//    }
-//}
